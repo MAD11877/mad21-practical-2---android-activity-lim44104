28788c7dfb0ac4e2e025a16efeca2b36305442e9
lim44104 <s10207455@connect.np.edu.sg>
2021-04-30 11:42:27 +0800

    Edit user.java

--- app/src/main/java/sg/edu/np/mad/madpractical/User.java
+++ app/src/main/java/sg/edu/np/mad/madpractical/User.java
@@ -1,10 +1,19 @@
 package sg.edu.np.mad.madpractical;
 
 public class User {
-    private String name;
-    private String description;
-    private int id;
-    private boolean followed;
+    public String name;
+    public String description;
+    public int id;
+    public boolean followed;
+
+    public User() {};
+
+    public User(String name, String description, int id, boolean followed) {
+        this.name = name;
+        this.description = description;
+        this.id = id;
+        this.followed = followed;
+    }
 
     public String getName() {
         return name;
